b089da04cc2040f0afa1ec83b3d9e18d439ae30f
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2022-11-18 14:33:53 -0300

    seção general - textos inseridos

--- src/pages/mainPage/components/general/General.js
+++ src/pages/mainPage/components/general/General.js
@@ -1,13 +1,24 @@
-import { Text } from '@chakra-ui/react'
+import { Text, Heading } from '@chakra-ui/react'
+import { GeneralContainer } from './styled'
 
 
 export const General = () => {
     return <>
-        <div>
-            Geral
+        <GeneralContainer>
+            <Heading>Yoga Mangala</Heading>
+            <Text>
+                Espaço voltado para práticas de Yoga, Meditação e Autoconhecimento. Relaxe e desfrute da prática de yoga e seus benefícios em um ambiente acolhedor, restaurando seu equilíbrio, harmonia e renovando suas energias.
+            </Text>
+            <Text>
+                Equilíbrio Corpo & Mente: Yoga é uma prática, um estilo de vida que integra o corpo e mente, desenvolvendo a consciência corporal, equilibrando as emoções e estimulando o fortalecimento e flexibilidade do corpo.
+            </Text>
+            <Text>
+                Fortalece o corpo físico: Praticar yoga é relaxa, corrige a postura, dá energia, aumenta a flexibilidade, tonifica os músculos e ajuda a prevenir doenças.
+            </Text>
+            <Text>
+                Vida Saudável: Fazer yoga com regularidade combate a depressão, insônia e ansiedade.
+            </Text>
 
-            <Text> Espaço voltado para práticas de Yoga, Meditação e Autoconhecimento. Relaxe e desfrute da prática de yoga e seus benefícios em um ambiente acolhedor, restaurando seu equilíbrio, harmonia e renovando suas energias.</Text>
-
-        </div>
+        </GeneralContainer>
     </>
 }
---
+++ src/pages/mainPage/components/general/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+import { primaryColor } from "../../../../constants/colors"
+
+export const GeneralContainer = styled.section`
+width:100%;
+height:50%;
+display: flex;
+flex-direction: column;
+gap: 2em;
+`
