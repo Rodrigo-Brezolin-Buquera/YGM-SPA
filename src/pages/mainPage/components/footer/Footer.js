715e28895cee1fa61055b88b9758c78a63b5fdb2
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2022-11-24 15:37:00 -0300

    footer -estilizado

--- src/pages/mainPage/components/footer/Footer.js
+++ src/pages/mainPage/components/footer/Footer.js
@@ -1,15 +1,20 @@
-import { FooterContainer } from "./styled"
-import { Text } from "@chakra-ui/react"
+import { FooterContainer, InfoContainer, SocialMediaContainer } from "./styled"
+import { Text, Image, Link } from "@chakra-ui/react"
 import { address, email, facebook, instagram, whatsapp } from "../../../../constants/texts/contactInformation"
+import White from "../../../../assets/logo/white.png"
+import FacebookWhite from "../../../../assets/socialMedia/FacebookWhite.png"
+import InstagramWhite from "../../../../assets/socialMedia/InstagramWhite.png"
 
 export const Footer = () => {
     return <FooterContainer>
-        <Text>{address}</Text>
-        <Text>{email}</Text>
-        <Text>{whatsapp}</Text>
-        <Text>{facebook}</Text>
-        <Text>{instagram}</Text>
-
-
+        <InfoContainer>
+            <Text color="white" fontSize="lg">Email: {email}</Text>
+            <Text color="white" fontSize="lg">Whatsapp:{whatsapp}</Text>
+        </InfoContainer>
+        <SocialMediaContainer>
+            <Link href={facebook}><Image src={FacebookWhite} boxSize="100px" alt="Facebook"/></Link>
+            <Link href={instagram}><Image src={InstagramWhite} boxSize="100px" alt="Instagram"/></Link>
+        </SocialMediaContainer>
+        <Image src={White} h='100px' w="200px" alt="Yoga Mangala Logo" />
     </FooterContainer>
 }
--- src/pages/mainPage/components/footer/styled.js
+++ src/pages/mainPage/components/footer/styled.js
@@ -3,8 +3,27 @@ import { primaryColor, secondaryColor } from "../../../../constants/colors"
 
 export const FooterContainer = styled.header`
 width:100%;
-height:70px;
+min-height:30%;
 background-color: ${secondaryColor};
 display: flex;
-gap:1em;
+align-items:center;
+justify-content: space-around;
+padding: 0 10em;
+`
+
+export const InfoContainer = styled.div`
+display: flex;
+flex-direction: column;
+gap:2em;
+justify-content:center;
+align-items:center;
+height: 100%;
+`
+
+export const SocialMediaContainer = styled.div`
+display: flex;
+gap:2em;
+justify-content:center;
+align-items:center;
+height: 100%;
 `
