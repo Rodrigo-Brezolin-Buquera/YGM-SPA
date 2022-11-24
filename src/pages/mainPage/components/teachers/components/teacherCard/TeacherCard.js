98b08944310cbfee6ebb7f1a0b85461311c0a83d
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2022-11-24 15:05:42 -0300

    secção professores - ajuste

--- src/pages/mainPage/components/teachers/components/teacherCard/TeacherCard.js
+++ src/pages/mainPage/components/teachers/components/teacherCard/TeacherCard.js
@@ -11,7 +11,7 @@ export const TeacherCard = ({image, description, name}) => {
   <Image
     objectFit='cover'
     maxW={{ base: '100%', sm: '200px' }}
-    src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+    src={image}
     alt='Caffe Latte'
   />
 
