0972923355503319f5edff4e9de68f946f093c63
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2022-11-24 19:34:17 -0300

    secção estilos -estilizada

--- src/pages/mainPage/components/yogaStyles/YogaStyles.js
+++ src/pages/mainPage/components/yogaStyles/YogaStyles.js
@@ -13,10 +13,6 @@ export const YogaStyles = () => {
         />)
 
     return <YogaStylesContainer>
-
         {StyleCardList}
-
-
-
     </YogaStylesContainer>
 }
--- src/pages/mainPage/components/yogaStyles/components/styleCards/StyleCard.js
+++ src/pages/mainPage/components/yogaStyles/components/styleCards/StyleCard.js
@@ -3,11 +3,11 @@ import { Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text } f
 export const StyleCard = ({image, text, title}) => {
   return(
 
-<Card maxW='sm'>
+<Card maxW='sm' backgroundColor={"whiteAlpha.400"} mt={"1em"}>
   <CardBody>
     <Image
-      src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-      alt='imagem'
+      src={image}
+      alt='Yoga posture'
       borderRadius='lg'
     />
     <Stack mt='6' spacing='3'>
