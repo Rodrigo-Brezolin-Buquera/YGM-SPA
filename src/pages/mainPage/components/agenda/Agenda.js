03071cbd4f213e14534431a97a818281c226c672
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2022-11-25 19:06:28 -0300

    seção agenda - retirada styledComp + ajustes incompletos

--- src/pages/mainPage/components/agenda/Agenda.js
+++ src/pages/mainPage/components/agenda/Agenda.js
@@ -1,14 +1,37 @@
 import { fridayClasses, mondayClasses, saturdayClasses, thursdayClasses, tuesdayClasses, wednesdayClasses } from "../../../../constants/texts/agendaInformation"
 import { DayCard } from "./components/DayCard/DayCard"
 import { AgendaContainer } from "./styled"
+import { Box } from "@chakra-ui/react"
+import Carousel1 from "../../../../assets/photos/Carousel1.png"
 
 export const Agenda = () => {
-    return <AgendaContainer>
-        <DayCard day={"Segunda"} classes={mondayClasses}/>
-        <DayCard day={"Terça"} classes={tuesdayClasses}/>
-        <DayCard day={"Quarta"}classes={wednesdayClasses} />
-        <DayCard day={"Quinta"} classes={thursdayClasses}/>
-        <DayCard day={"Sexta"} classes={fridayClasses}/>
-        <DayCard day={"Sábado"} classes={saturdayClasses}/>
-    </AgendaContainer>
+    return (
+        <Box
+            display={"flex"}
+            flexDirection="column"
+            justifyContent={"center"}
+            alignItems={"center"}
+            gap="1em"
+            paddingTop={"1em"}
+            bgImage={Carousel1} // trocar de foto e deixar opaca no canva
+            h="50em"
+        >
+            <Box
+                display={"flex"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                gap="1em"
+            >
+                <DayCard day={"Segunda"} classes={mondayClasses} />
+                <DayCard day={"Terça"} classes={tuesdayClasses} />
+                <DayCard day={"Quarta"} classes={wednesdayClasses} />
+                <DayCard day={"Quinta"} classes={thursdayClasses} />
+                <DayCard day={"Sexta"} classes={fridayClasses} />
+                <DayCard day={"Sábado"} classes={saturdayClasses} />
+            </Box>
+
+            // faixa amarela: "agende aula experimental"
+        </Box>
+
+    )
 }
