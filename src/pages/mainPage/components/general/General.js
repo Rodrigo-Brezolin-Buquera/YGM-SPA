ecd8b33830dec1a9e70456627d66a1504f5ec897
RODRIGO BREZOLIN BUQUERA <81428197+Rodrigo-Brezolin-Buquera@users.noreply.github.com>
2022-11-25 13:38:55 -0300

    secção general - estilização

--- src/pages/mainPage/components/general/General.js
+++ src/pages/mainPage/components/general/General.js
@@ -1,25 +1,39 @@
 import { Text, Heading, Box } from '@chakra-ui/react'
-import { heading1, subtitle, text1 } from '../../../../constants/texts/generalInformation'
+import { heading1, heading2, heading3, subtitle, text1, text2, text3 } from '../../../../constants/texts/generalInformation'
 import { InformationCard } from './components/InfomationCard'
-import { GeneralContainer } from './styled'
 import { AiFillHeart } from "react-icons/ai";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { GiWeightLiftingUp } from "react-icons/gi";
+
 
 export const General = () => {
     return <>
-        <GeneralContainer>
+        <Box w={"100%"} h={"100%"} display="flex" flexDirection={"column"} alignItems="center" paddingTop={"2em"} gap="1em">
             <Heading>Yoga Mangala</Heading>
-            <Heading fontSize={"sm"}>
-               {subtitle}
-            </Heading>
-            <Box display={"flex"}>
+            <Text textAlign="center" fontSize={"xl"} padding="1em">
+                {subtitle}
+            </Text>
+            <Box display={"flex"} justifyContent="space-evenly" height="300px">
                 <InformationCard
-                title={heading1}
-                text={text1}
-                Icon={<AiFillHeart size={"50px"}/>}
-                color={"tomato"}
+                    title={heading1}
+                    text={text1}
+                    Icon={<AiFillHeart size={"60px"} />}
+                    color={"tomato"}
+                />
+                <InformationCard
+                    title={heading2}
+                    text={text2}
+                    Icon={<GiWeightLiftingUp size={"60px"} />}
+                    color={"gold"}
+                />
+                <InformationCard
+                    title={heading3}
+                    text={text3}
+                    Icon={<BsFillMoonStarsFill size={"60px"} />}
+                    color={"teal"}
                 />
             </Box>
-        
-        </GeneralContainer>
+
+        </Box>
     </>
 }
--- src/pages/mainPage/components/general/components/InfomationCard.js
+++ src/pages/mainPage/components/general/components/InfomationCard.js
@@ -2,12 +2,12 @@ import { Text, Heading, Box, Circle,  } from '@chakra-ui/react'
 
 export const InformationCard = ({ title, text, Icon, color }) => {
     return (
-        <Box display={"flex"} flexDirection="column" >
+        <Box display={"flex"} flexDirection="column" w="30%" justifyContent={"flex-start"} alignItems={"center"} gap={"0.5em"} >
             <Circle size='100px' bg={color} color="white">
                {Icon}            
             </Circle>
-            <Heading>{title}</Heading>
-            <Text>{text}</Text>
+            <Heading fontSize={"3xl"} textAlign="center">{title}</Heading>
+            <Text textAlign="center" >{text}</Text>
         </Box>
     )
 }
